5c8eb3ae444da9a8df9b6aeda9073212b1792305
Stephan Bezoen <stephan@mediamonks.com>
2017-05-19 10:45:23 +0200

    - cleanup

--- app/src/main/java/nl/acidcats/tumblrlikes/data/repo/photo/store/filters/FilterOption.java
+++ app/src/main/java/nl/acidcats/tumblrlikes/data/repo/photo/store/filters/FilterOption.java
@@ -1,10 +1,6 @@
 package nl.acidcats.tumblrlikes.data.repo.photo.store.filters;
 
-import org.greenrobot.greendao.query.CountQuery;
-import org.greenrobot.greendao.query.QueryBuilder;
-
 import nl.acidcats.tumblrlikes.data.vo.db.PhotoEntity;
-import nl.acidcats.tumblrlikes.data.vo.db.PhotoEntityDao;
 
 /**
  * Created by stephan on 17/05/2017.
