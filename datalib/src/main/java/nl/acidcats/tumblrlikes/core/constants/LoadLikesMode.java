e37fbe0b9b765c1acddfc1d62afb9f3d4b123c3e
Stephan Bezoen <stephan@mediamonks.com>
2018-10-24 10:49:52 +0200

    - more conversion to Kotlin

--- datalib/src/main/java/nl/acidcats/tumblrlikes/core/constants/LoadLikesMode.java
+++
@@ -1,8 +0,0 @@
-package nl.acidcats.tumblrlikes.core.constants;
-
-/**
- * Created on 01/10/2018.
- */
-public enum LoadLikesMode {
-    FRESH, CONTINUED
-}
---
+++ datalib/src/main/java/nl/acidcats/tumblrlikes/core/constants/LoadLikesMode.kt
@@ -0,0 +1,8 @@
+package nl.acidcats.tumblrlikes.core.constants
+
+/**
+ * Created on 01/10/2018.
+ */
+enum class LoadLikesMode {
+    FRESH, CONTINUED
+}
