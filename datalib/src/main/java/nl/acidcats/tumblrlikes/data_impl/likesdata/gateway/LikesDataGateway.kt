c59c9167a370e5585db4b2428f12d4abf1e1cf9f
Stephan Bezoen <stephan@mediamonks.com>
2018-10-25 10:39:25 +0200

    - more conversion to Kotlin

---
+++ datalib/src/main/java/nl/acidcats/tumblrlikes/data_impl/likesdata/gateway/LikesDataGateway.kt
@@ -0,0 +1,12 @@
+package nl.acidcats.tumblrlikes.data_impl.likesdata.gateway
+
+import nl.acidcats.tumblrlikes.data_impl.likesdata.models.TumblrLikeVO
+import rx.Observable
+
+/**
+ * Created by stephan on 28/03/2017.
+ */
+
+interface LikesDataGateway {
+    fun getLikes(blogName: String, count: Int, beforeTime: Long): Observable<List<TumblrLikeVO>>
+}
