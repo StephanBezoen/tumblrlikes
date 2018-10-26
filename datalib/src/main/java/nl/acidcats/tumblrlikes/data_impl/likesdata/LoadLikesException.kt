ae6d334bc7ed9f9f9e49b01e83648e51603a433d
Stephan Bezoen <stephan@mediamonks.com>
2018-10-26 10:34:53 +0200

    - moved more classes to kotlin folder

--- datalib/src/main/java/nl/acidcats/tumblrlikes/data_impl/likesdata/LoadLikesException.kt
+++
@@ -1,6 +0,0 @@
-package nl.acidcats.tumblrlikes.data_impl.likesdata
-
-/**
- * Created on 12/07/2018.
- */
-class LoadLikesException internal constructor(val code: Int) : Exception()
--- datalib/src/main/java/nl/acidcats/tumblrlikes/util/security/SecurityHelper.kt
+++
@@ -1,9 +0,0 @@
-package nl.acidcats.tumblrlikes.util.security
-
-/**
- * Created by stephan on 13/04/2017.
- */
-
-interface SecurityHelper {
-    fun getHash(text: String): String
-}
