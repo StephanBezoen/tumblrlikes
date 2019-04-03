84e4439f92989db68c593ac6a0539a076c092f79
Stephan Bezoen <stephan@mediamonks.com>
2019-04-03 10:23:28 +0200

    - various library updates

--- datalib/src/test/java/nl/acidcats/tumblrlikes/tests/ParseLikeTest.kt
+++ datalib/src/test/java/nl/acidcats/tumblrlikes/tests/ParseLikeTest.kt
@@ -7,35 +7,48 @@ import nl.acidcats.tumblrlikes.data_impl.likesdata.transformers.EmptyTransformer
 import nl.acidcats.tumblrlikes.data_impl.likesdata.transformers.TransformerProvider
 import nl.acidcats.tumblrlikes.data_impl.likesdata.transformers.TumblrLikePhotoTransformer
 import nl.acidcats.tumblrlikes.data_impl.likesdata.transformers.TumblrLikeTextTransformer
-import org.junit.Assert.*
-import org.junit.Test
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.reflect.KClass
 
 /**
  * Created on 09/11/2018.
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ParseLikeTest {
 
+    companion object {
+        @JvmStatic
+        fun testData(): List<Arguments> = listOf(
+            Arguments.of(TumblrLikeJsonMock.TEXT_LIKE, 179516097297L, TumblrLikeTextTransformer::class.java, "https://66.media.tumblr.com/32b744bef025114c8b51bb3d5e5f078f/tumblr_phb7tsO4Dj1uj0hhs_1280.jpg", true),
+            Arguments.of(TumblrLikeJsonMock.PHOTO_LIKE, 179516206403L, TumblrLikePhotoTransformer::class.java, "https://66.media.tumblr.com/30e407c70cd625e02af04e49b8c96ed5/tumblr_p928p09OP11qjx7gho1_500.jpg", false)
+        )
+    }
 
-    @Test
-    fun testParseTextJson() {
-        val tumblrLike = getTumblrLikeVO(TumblrLikeJsonMock.TEXT_LIKE)
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun `test mocking Json`(json: String, likeId: Long, cls: Class<*>, photoUrl:String, checkBodyText:Boolean) {
+        val tumblrLike = getTumblrLikeVO(json)
 
-        assertNotNull(tumblrLike)
-        assertEquals(tumblrLike!!.id, 179516097297L)
-        assertNotNull(tumblrLike.bodyText)
+        assertAll("testing like",
+            { assertNotNull(tumblrLike) },
+            { assertEquals(tumblrLike!!.id, likeId) },
+            { if (checkBodyText) assertNotNull(tumblrLike!!.bodyText) }
+        )
 
-        val transformer = TransformerProvider().getTransformer(tumblrLike)
-
-        assertNotNull(transformer)
-        assertTrue(transformer is TumblrLikeTextTransformer)
-        assertTrue(transformer!!.accepts(tumblrLike))
+        val transformer = TransformerProvider().getTransformer(tumblrLike!!)
+        assertEquals(transformer::class.java, cls)
+        assertTrue(transformer.accepts(tumblrLike))
 
         val photos = transformer.transform(tumblrLike)
         assertEquals(photos.size, 1)
 
         val photo = photos[0]
         assertNotNull(photo.url)
-        assertEquals(photo.url, "https://66.media.tumblr.com/32b744bef025114c8b51bb3d5e5f078f/tumblr_phb7tsO4Dj1uj0hhs_1280.jpg")
+        assertEquals(photo.url, photoUrl)
     }
 
     @Test
@@ -54,35 +67,16 @@ class ParseLikeTest {
     }
 
     @Test
-    fun testParsePhotoJson() {
-        val tumblrLike = getTumblrLikeVO(TumblrLikeJsonMock.PHOTO_LIKE)
-
-        assertNotNull(tumblrLike)
-        assertEquals(tumblrLike!!.id, 179516206403L)
-
-        val transformer = TransformerProvider().getTransformer(tumblrLike)
-        assertNotNull(transformer)
-        assertTrue(transformer is TumblrLikePhotoTransformer)
-        assertTrue(transformer!!.accepts(tumblrLike))
-
-        val photos = transformer.transform(tumblrLike)
-        assertEquals(photos.size, 1)
-
-        val photo = photos[0]
-        assertNotNull(photo.url)
-        assertEquals(photo.url, "https://66.media.tumblr.com/30e407c70cd625e02af04e49b8c96ed5/tumblr_p928p09OP11qjx7gho1_500.jpg")
-    }
-
-    @Test
     fun testParseVideoJson() {
         val tumblrLike = getTumblrLikeVO(TumblrLikeJsonMock.VIDEO_LIKE)
 
         assertNotNull(tumblrLike)
         assertEquals(tumblrLike!!.id, 179521270301L)
+
         val transformer = TransformerProvider().getTransformer(tumblrLike)
         assertNotNull(transformer)
         assertTrue(transformer is EmptyTransformer)
-        assertTrue(transformer!!.accepts(tumblrLike))
+        assertTrue(transformer.accepts(tumblrLike))
 
         val photos = transformer.transform(tumblrLike)
         assertEquals(photos.size, 0)
